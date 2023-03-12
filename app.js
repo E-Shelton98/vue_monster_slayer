c8fb47655ab638481053d6b50e27f67c13eeb656
E-Shelton98 <erik.shelton92@gmail.com>
2023-03-12 04:49:44 -0400

    designed basic multi-enemy choice system that gives different enemies with different attributes based on player level.

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-function getRandomValue(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min
+function getRandomValue(min, max, modifier) {
+  if (modifier) {
+    return Math.floor(Math.random() * (max - min)) + min * modifier
+  } else {
+    return Math.floor(Math.random() * (max - min)) + min
+  }
 }
 
 const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
-      monsterHealth: 100,
+      playerMaxHealth: 100,
+      playerDamageModifier: 1,
+      playerHealingModifier: 1,
+      playerXP: 0,
+      playerLevel: 1,
+      monsterHealth: 50,
+      monsterMaxHealth: 50,
+      monsterDamageModifier: 0.5,
+      monsterHealingModifier: 1,
+      monsterCanHeal: false,
+      monsterXPValue: 15,
+      monsterType: 'Slime',
+      monsterTypeValues: {
+        Slime: 0,
+        Goblin: 1,
+        Ranger: 2,
+        Apprentice: 3,
+        Orc: 4,
+        Wizard: 5,
+        Ogre: 6,
+      },
       currentRound: 0,
       winner: null,
-      logMessages: []
+      logMessages: [],
     }
   },
   computed: {
@@ -17,13 +41,13 @@ const app = Vue.createApp({
       if (this.monsterHealth < 0) {
         return { width: '0%' }
       }
-      return { width: this.monsterHealth + '%' }
+      return { width: (this.monsterHealth / this.monsterMaxHealth * 100) + '%' }
     },
     playerBarStyles() {
-        if (this.playerHealth < 0) {
-            return { width: '0%'}
-        }
-      return { width: this.playerHealth + '%' }
+      if (this.playerHealth < 0) {
+        return { width: '0%' }
+      }
+      return { width: (this.playerHealth / this.playerMaxHealth * 100) + '%' }
     },
     canSpecialAttack() {
       return this.currentRound % 3 !== 0
@@ -42,22 +66,100 @@ const app = Vue.createApp({
         this.winner = 'draw'
       } else if (value <= 0) {
         this.winner = 'player'
+        this.playerXP += this.monsterXPValue
+        console.log('Player XP: ', this.playerXP)
       }
     },
+    playerXP(value) {
+        if(value > 100) {
+            this.levelUpPlayer('damage')
+        }
+    }
   },
   methods: {
     startGame() {
-        this.playerHealth = 100
-        this.monsterHealth = 100
-        this.winner = null
-        this.currentRound = 0
-        this.logMessages = []
+      this.playerHealth = this.playerMaxHealth
+      this.setMonsterType(this.playerLevel)
+      this.winner = null
+      this.currentRound = 0
+      this.logMessages = []
+    },
+    setMonsterType(playerLevel) {
+      let monsterValue = null
+      if (playerLevel <= 4) {
+        monsterValue = getRandomValue(0, 1)
+      } else if (playerLevel >= 5 && playerLevel < 9) {
+        monsterValue = getRandomValue(0, 2)
+      } else if (playerLevel >= 9 && playerLevel < 13) {
+        monsterValue = getRandomValue(1, 3)
+      } else if (playerLevel >= 13 && playerLevel < 17) {
+        monsterValue = getRandomValue(2, 4)
+      } else if (playerLevel >= 17 && playerLevel < 21) {
+        monsterValue = getRandomValue(3,5)
+      } else if (playerLevel >=21 && playerLevel <= 24) {
+        monsterValue = getRandomValue(4,5)
+      } else if (playerLevel >= 25) {
+        monsterValue = 6
+      }
+      let monsterTypes = Object.keys(this.monsterTypeValues)
+      this.monsterType = monsterTypes[monsterValue]
+      if (this.monsterType === 'Slime') {
+        this.monsterHealth = 50
+        this.monsterMaxHealth = 50
+        this.monsterDamageModifier = 0.5
+        this.monsterHealingModifier = 1
+        this.monsterXPValue = 15
+        this.monsterCanHeal = false
+      } else if (this.monsterType === 'Goblin') {
+        this.monsterHealth = 75
+        this.monsterMaxHealth = 75
+        this.monsterDamageModifier = 1
+        this.monsterHealingModifier = 1
+        this.monsterXPValue = 35
+        this.monsterCanHeal = false
+      } else if (this.monsterType === 'Ranger') {
+        this.monsterMaxHealth = getRandomValue(90, 110)
+        this.monsterHealth = this.monsterMaxHealth
+        this.monsterDamageModifier = 1.2
+        this.monsterHealingModifier = 1
+        this.monsterXPValue = 50
+        this.monsterCanHeal = false
+      } else if (this.monsterType === 'Apprentice') {
+        this.monsterMaxHealth = getRandomValue(65, 80)
+        this.monsterHealth = this.monsterMaxHealth
+        this.monsterDamageModifier = 1.3
+        this.monsterHealingModifier = 1
+        this.monsterXPValue = 60
+        this.monsterCanHeal = true
+      } else if (this.monsterType === 'Wizard') {
+        this.monsterMaxHealth = getRandomValue(70, 95)
+        this.monsterHealth = this.monsterMaxHealth
+        this.monsterDamageModifier = 1.6
+        this.monsterHealingModifier = 1.2
+        this.monsterXPValue = 80
+        this.monsterCanHeal = true
+      } else if (this.monsterType === 'Orc') {
+        this.monsterMaxHealth = getRandomValue(100, 120)
+        this.monsterHealth = this.monsterMaxHealth
+        this.monsterDamageModifier = 1.4
+        this.monsterHealingModifier = 1
+        this.monsterXPValue = 75
+        this.monsterCanHeal = false
+      } else if (this.monsterType === 'Ogre') {
+        this.monsterMaxHealth = 200
+        this.monsterHealth = 200
+        this.monsterDamageModifier = 1.7
+        this.monsterHealingModifier = 1
+        this.monsterXPValue = 90
+        this.monsterCanHeal = false
+      }
     },
     attackMonster() {
       this.currentRound++
       const attackValue = getRandomValue(5, 12)
       this.monsterHealth -= attackValue
       this.addLogMessage('player', 'attacks', attackValue)
+      console.log(this.monsterHealth, this.monsterMaxHealth)
       this.attackPlayer()
     },
     attackPlayer() {
@@ -84,21 +186,32 @@ const app = Vue.createApp({
       this.attackPlayer()
     },
     surrender() {
-        this.winner = 'monster'
-        this.addLogMessage('player', 'surrenders', null)
+      this.winner = 'monster'
+      this.addLogMessage('player', 'surrenders', null)
     },
     addLogMessage(who, what, value) {
-        if (what === 'surrenders')
+      if (what === 'surrenders')
         this.logMessages.unshift({
-            actionBy: who,
-            actionType: what
+          actionBy: who,
+          actionType: what,
         })
-        this.logMessages.unshift({
-            actionBy: who,
-            actionType: what,
-            actionValue: value
-        })
-    }
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      })
+    },
+    levelUpPlayer(choice) {
+        console.log('hello')
+      this.playerXP -= 100
+      if (choice === 'damage') {
+        this.playerDamageModifier += 1
+      } else if (choice === 'maxHealth') {
+        this.playerMaxHealth
+      } else if (choice === 'healingSkill') {
+        this.playerHealingModifier += 1
+      }
+    },
   },
 })
 
